23beab9eab0c47ddd1206634f1c66ea680c3d4c7
loihnt05 <23520869@gm.uit.edu.vn>
2025-07-17 08:55:28 +0700

    apollo same same usequery

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import BookList from "./components/BookList";
+import ApolloClient from "apollo-boost";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+});
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+      <div className="App" id="main">
+        <h1>Ninja's Reading List</h1>
+        <BookList />
+      </div>
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
